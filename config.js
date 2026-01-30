4e1ec64afc7a33e483ad966a18c8cb427af399b4
Mohammed Fadil <91049862+mfadi1@users.noreply.github.com>
2026-01-30 21:14:07 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+    // Person's name
+    name: "FD",
+    
+    // Birth date
+    birthYear: 2005,    // Year of birth
+    birthMonth: 10,     // October
+    birthDay: 23,
+    
+    // Custom message shown on birthday
+    birthdayMessage: "Happy Birthday. You’re getting old af. Another year gone, another wrinkle earned. Make this one count."
+};
